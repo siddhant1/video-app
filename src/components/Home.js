b0564429d8605c43842ccae72f5ec5969c7cbc49
shivanshBTW <stshivansh9@gmail.com>
2020-09-30 19:41:44 +0530

    iteach branding

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 const Home = props =>
   <div className="home">
     <div>
-      <h1 itemProp="headline">Webrtc Video Room</h1>
-      <p>Please enter a room name.</p>
-      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
+      <h1 itemProp="headline">ITeach Classroom</h1>
+      <p>Please enter a classroom's name.</p>
+      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Classroom name should only contain letters or numbers."/>
       <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
       <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link>
-      { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
+      { props.rooms.length !== 0 && <div>Recently visited classrooms:</div> }
       { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
     </div>
   </div>;
